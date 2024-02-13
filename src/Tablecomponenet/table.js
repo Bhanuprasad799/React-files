b07fa6db1ee0027e8889cedcaeaec1f451a646cd
Bhanuprasad799 <bhanuprasad93996@gmail.com>
2024-02-13 21:59:25 +0530

    components sturucture

---
+++ src/Tablecomponenet/table.js
@@ -0,0 +1,47 @@
+const TableComponent=()=>{
+    
+    return(
+<div>
+
+<table style={{border:'1px solid black'}}>
+<tr style={{border:'1px solid black'}} >
+
+    <th>birds</th>
+    <th>animals</th>
+    <th>pets</th>
+</tr>
+
+
+
+
+
+<tr style={{border:'1px solid black'}}>
+    <td>
+
+<img src='https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?auto=compress&cs=tinysrgb&w=600'  alt='' style={{width:300}}/>
+    </td>
+    <td>
+<img  src="https://images.pexels.com/photos/135940/pexels-photo-135940.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" style={{width:300}}/>
+
+    </td>
+
+
+    <td>
+<img   src="https://images.pexels.com/photos/45853/grey-crowned-crane-bird-crane-animal-45853.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" style={{width:300}} />
+
+    </td>
+</tr>
+
+
+
+
+</table>
+
+</div>
+
+
+
+    )
+
+}
+export default TableComponent
