cfeeec56727216adcebe40f1cca5ffe875de532b
Bhanuprasad799 <bhanuprasad93996@gmail.com>
2024-02-16 21:39:46 +0530

    jsx

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 
 // import TableComponent from './Tablecomponenet/table';
 import Profile from "./components/functional-components/jsx/jsx";
+import './App.css';
+import Product from "./components/functional-components/jsx/jsx2";
 function App() {
   return (
     <div className="App">
      {/* <TableComponent/> */}
 
-     <Profile></Profile>
+     {/* <Profile></Profile> */}
+     <Product/>
     </div>
   );
 }
